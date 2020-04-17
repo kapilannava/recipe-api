2012737aaa77be4bd476060c814907639731a9b3
Kapilan Navaratnam <kapilan@syomortgage.com>
2020-04-17 04:22:47 -0400

    Updates

---
+++ precache-manifest.ee558dad0bc75117581dbded24e961bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8bcf34125b588c6fff2d6f75d9faa6d",
+    "url": "/recipe-api/index.html"
+  },
+  {
+    "revision": "5f31f34c869052aa6c87",
+    "url": "/recipe-api/static/css/main.651aa0f5.chunk.css"
+  },
+  {
+    "revision": "5d9d24352b01a03ef3e7",
+    "url": "/recipe-api/static/js/2.3664ffc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-api/static/js/2.3664ffc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f31f34c869052aa6c87",
+    "url": "/recipe-api/static/js/main.1cf6973d.chunk.js"
+  },
+  {
+    "revision": "ee147a37e15687f610f7",
+    "url": "/recipe-api/static/js/runtime-main.108100b4.js"
+  }
+]);
